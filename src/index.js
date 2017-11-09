058cf00f8fa41c8016aeba4714d74989afd04b82
Murithi <murithier.2006@gmail.com>
2017-11-09 16:21:19 +0300

    semantic ui set up

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
